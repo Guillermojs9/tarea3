f82ba90aae48b16bc89841a8c9bc7f2317520e52
Guillermo <grobles64@ieszaidinvergeles.org>
2024-12-13 18:46:54 +0100

    Primer fetch

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+function datos() {
+  const datos = fetch("http://192.168.1.136:3000/items")
+    .then(response => response.json())
+    .then(data => console.log(data)); 
+}
+datos()
 export default function App() {
   return (
     <View style={styles.container}>
